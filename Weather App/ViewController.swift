a4c1b49d106797bd003c4ceff1a76fcd34e6c22e
Davi Paiva <davialexandre_dap@hotmail.com>
2024-11-22 00:19:32 -0300

    add contraints in View Controller

--- Weather App/ViewController.swift
+++ Weather App/ViewController.swift
@@ -8,12 +8,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+    // MARK: - Global Variables
+    private lazy var customView:UIView = {
+        let view = UIView()
+        view.backgroundColor = .darkGray
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    
+    // MARK: - Cycles
+    // setup after loading the view in the first time.
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemCyan
+        setupView()
+    }
+    
+    // setup befor every time the view will appear
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    // setup every time the view appear
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        	
+    }
+    
+    // setup before window diasappear
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    // setup when window dissapear
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
     }
 
-
+    // MARK: - Custom View
+    
+    private func setupView(){
+        setHierarchy()
+        setContraints()
+    }
+    
+    private func setHierarchy(){
+        view.addSubview(customView)
+    }
+    
+    private func setContraints(){
+        customView.topAnchor.constraint(equalTo:view.safeAreaLayoutGuide.topAnchor,constant: 100).isActive = true
+        customView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50).isActive = true
+        customView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50).isActive = true
+        customView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100).isActive = true
+    }
+    
 }
 
