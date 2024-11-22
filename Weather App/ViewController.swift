8ac628e0d6dfb2f32d6752ea9738a6fad70f417d
Davi Paiva <davialexandre_dap@hotmail.com>
2024-11-22 19:24:09 -0300

    add: temp and sun icon

--- Weather App/ViewController.swift
+++ Weather App/ViewController.swift
@@ -10,19 +10,55 @@ import UIKit
 class ViewController: UIViewController {
   
     // MARK: - Global Variables
-    private lazy var customView:UIView = {
+    private lazy var backGroundView:UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "background")
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var headerView: UIView = {
         let view = UIView()
-        view.backgroundColor = .darkGray
+        view.backgroundColor = .white
         view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 20
         return view
     }()
     
+    private lazy var cityLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.text = "Aquiraz"
+        label.textAlignment = .center
+        label.textColor = UIColor(named: "primaryColor")
+        return label
+    }()
+    
+    private lazy var temperatureLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 50,weight: .bold)
+        label.text = "25°C"
+        label.textAlignment = .left
+        label.textColor = UIColor(named: "primaryColor")
+        return label
+    }()
+    
+    private lazy var weatherIcon: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "sunIcon")
+        
+        return imageView
+    }()
     
     // MARK: - Cycles
     // setup after loading the view in the first time.
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemCyan
+        view.backgroundColor = .white
         setupView()
     }
     
@@ -50,19 +86,52 @@ class ViewController: UIViewController {
     // MARK: - Custom View
     
     private func setupView(){
+        // set backgound View
         setHierarchy()
         setContraints()
+        
+        // set headerView
     }
     
     private func setHierarchy(){
-        view.addSubview(customView)
+        view.addSubview(backGroundView)
+        view.addSubview(headerView)
+        
+        headerView.addSubview(cityLabel)
+        headerView.addSubview(temperatureLabel)
+        headerView.addSubview(weatherIcon)
     }
     
     private func setContraints(){
-        customView.topAnchor.constraint(equalTo:view.safeAreaLayoutGuide.topAnchor,constant: 100).isActive = true
-        customView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50).isActive = true
-        customView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50).isActive = true
-        customView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100).isActive = true
+        // backgound contraints
+        backGroundView.topAnchor.constraint(equalTo:view.topAnchor).isActive = true
+        backGroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        backGroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        backGroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        // header contraints
+        headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 59).isActive = true
+        headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 35).isActive = true
+        headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -35).isActive = true
+        headerView.heightAnchor.constraint(equalToConstant: 169).isActive = true
+        
+        // city contraints
+        cityLabel.topAnchor.constraint(equalTo: headerView.topAnchor, constant: 15).isActive = true
+        cityLabel.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 15).isActive = true
+        cityLabel.trailingAnchor.constraint(equalTo: headerView.trailingAnchor, constant: -20).isActive = true
+        cityLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        
+        // temperature contraints
+        temperatureLabel.topAnchor.constraint(equalTo: cityLabel.bottomAnchor, constant: 21).isActive = true
+        temperatureLabel.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 26).isActive = true
+        
+        // weather icon contraints
+        weatherIcon.widthAnchor.constraint(equalToConstant: 86).isActive = true
+        weatherIcon.heightAnchor.constraint(equalToConstant: 86).isActive = true
+        weatherIcon.trailingAnchor.constraint(equalTo: headerView.trailingAnchor,constant: -26).isActive = true
+        weatherIcon.centerYAnchor.constraint(equalTo: temperatureLabel.centerYAnchor).isActive = true
+        weatherIcon.leadingAnchor.constraint(equalTo: temperatureLabel.trailingAnchor,constant: 15).isActive = true
+        
     }
     
 }
